5870c96c448f64917e5acf47fa339c4f24c3034b
xiaozhou <109784503+zcdyhm@users.noreply.github.com>
2024-07-24 00:50:09 +0800

    Add files via upload

---
+++ src/ClashRoyale.Battles/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ClashRoyale.Battles
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.Title = "ClashRoyale Battle Server Emulator";
+
+            Console.WriteLine(
+"\n ______________           ______________\r\n/\\__________   \\         /\\   ________  \\\r\n\\/_________/   /         \\ \\  \\______/\\  \\\r\n          /   /      __   \\ \\  \\_____\\/  /\r\n         /   /      /\\_\\   \\ \\   ______  \\\r\n        /   /       \\/_/    \\ \\  \\____/\\  \\\r\n       /   /                 \\ \\  \\   \\ \\  \\\r\n      /   /__________         \\ \\  \\   \\ \\  \\___\r\n     /\\______________\\         \\ \\__\\   \\ \\_____\\\r\nHi:  \\/______________/          \\/__/    \\/_____/\n\n-光临-\n");
+
+            Resources.Initialize();
+
+            Console.Read();
+        }
+    }
+}
